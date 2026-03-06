39ffc0b28e08d42707211e3c6643f119500b8da2
michimichimchi <m.zerner0@gmail.com>
2026-03-06 16:06:24 +0100

    database init

--- backend.py
+++ backend.py
@@ -1,15 +1,42 @@
 import sqlite3
 
-DB_NAME = "POW Kasse"
+DB = "POW_Kasse.db"
 
-def create_table():
-    pass
+def create_db():
+    purposes = ["Noten", "Essen", "Trinken", "Geschenke", "Spenden", "Auftritt", "Sonstiges"]
+
+    with sqlite3.connect(DB) as con:
+        cur = con.cursor()
+        cur.execute("PRAGMA foreign_keys = ON;")
+        cur.execute("""CREATE TABLE IF NOT EXISTS transaction_purpose (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        name TEXT UNIQUE) """)
+        cur.execute("""CREATE TABLE IF NOT EXISTS transactions (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    date TEXT,
+                    amount REAL,
+                    purpose_id INTEGER,
+                    FOREIGN KEY (purpose_id) REFERENCES transaction_purpose(id)
+                    ) """)
+        for purpose in purposes:
+            cur.execute(f"INSERT OR IGNORE INTO transaction_purpose (name) VALUES '{purpose}'")
 
 def new_transaction():
-    pass
+    with sqlite3.connect(DB) as con:
+        cur = con.cursor()
+        date = input("Datum (DD.MM.YYYY): ")
+        amount = float(input("Betrag: "))
 
 def view_transactions():
     pass
 
 def view_balance():
-    pass
+    with sqlite3.connect(DB) as con:
+        cur = con.cursor()
+        res = cur.execute("SELECT SUM(amount) FROM transactions;")
+        print(res.fetchone())
+
+if __name__ == "__main__":
+    create_db()
+    new_transaction()
+    view_transactions()
